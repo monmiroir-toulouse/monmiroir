f5da4922905f6c22b4e02f00f70a49fc14c50f3e
monmiroir-toulouse <mohamedanaya@hotmail.fr>
2026-05-03 07:55:23 +0200

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -21,7 +21,7 @@ module.exports = async function handler(req, res) {
       body: JSON.stringify({
         text: cleanText,
        model_id: 'eleven_turbo_v2_5',
-        voice_settings: { stability: 0.5, similarity_boost: 0.8 }
+        voice_settings: { stability: 0.75, similarity_boost: 0.75 }
       })
     });
     if (!response.ok) {
